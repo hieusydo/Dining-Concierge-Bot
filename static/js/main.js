300a9d4f3380e1a3c53f3766f7cd50feae482f44
hieusydo <hieusydo@gmail.com>
2019-03-12 14:55:31 -0400

    Refactored static code

--- static/js/main.js
+++ static/js/main.js
@@ -2,67 +2,11 @@ $(document).ready(function() {
     handleAuth();
 });
 
-function handleInputSubmit(apigClient) {
-    var outputArea = $("#chat-output");
-
-    $("#user-input-form").submit(function(e) {
-        var message = $("#user-input").val();
-        // console.log(message);
-        e.preventDefault();
-
-        outputArea.append(`
-        <div class='bot-message'>
-        <div class='message'>
-        ${message}
-        </div>
-        </div>
-        `);
-        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-        $("#user-input").val("");
-
-        var body = {
-            "text": message,
-            "timestamp": Date.now()
-        };
-        var params = {};
-        var additionalParams = {
-            headers: {},
-            queryParams: {}
-        };
-        apigClient.chatbotPost(params, body, additionalParams)
-        .then(function(result){
-            console.log(result.status);
-            console.log(result.data);
-
-            var reply = result.data.body.message;
-
-            outputArea.append(`
-            <div class='user-message'>
-            <div class='message'>
-            ${reply}
-            </div>
-            </div>
-            `);
-
-        }).catch( function(err){
-            console.log(err)
-        });
-    });
-}
-
-function tokenizeUrl() {
-    var vars = {};
-    var parts = window.location.href.replace(/[#?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
-
 function handleAuth() {
     // Verify token from url
     urlTokens = tokenizeUrl()
 
-    /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+    // Use the idToken for Logins Map when Federating User Pools with identity pools
     var idToken = urlTokens["id_token"];
 
     // Add the User's Id Token to the Cognito credentials login map.
@@ -78,31 +22,89 @@ function handleAuth() {
     AWS.config.credentials.refresh((error) => {
         if (error) {
             console.error(error);
+            $("#chat-scroll").append(`
+            <div class="alert alert-danger" role="alert">
+                Unauthorized. Please sign in <a href="https://nyu-cloud-proj1.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=369d3nt3scah7fssv3aedq9h8m&redirect_uri=https://s3.amazonaws.com/nyu-cloud-proj1/index.html">here</a>
+            </div>
+            `)
+            $("#user-input").prop("disabled", true);
         } else {
-            // Instantiate aws sdk service objects now that the credentials have been updated.
-            console.log('Successfully authenticated!');
-
-            // Make the call to obtain credentials
+            // Fetch credentials to make authorized API calls
             AWS.config.credentials.get(function(){
 
-                // Credentials will be available when this function is called.
                 var accessKeyId = AWS.config.credentials.accessKeyId;
                 var secretAccessKey = AWS.config.credentials.secretAccessKey;
                 var sessionToken = AWS.config.credentials.sessionToken;
 
-                // console.log(accessKeyId, secretAccessKey)
-
+                // Init apigClient object with the credentials
                 var apigClient = apigClientFactory.newClient({
                     accessKey: accessKeyId,
                     secretKey: secretAccessKey,
                     sessionToken: sessionToken
                 });
 
-                // console.log(apigClient);
-
                 handleInputSubmit(apigClient);
-
             });
         }
     });
 }
+
+function handleInputSubmit(apigClient) {
+    $("#user-input-form").submit(function(e) {
+        var message = $("#user-input").val();
+        e.preventDefault();
+        addUserMsgUI(message);
+        sendPost(apigClient, message);
+    });
+}
+
+function sendPost(apigClient, message) {
+    var body = {
+        "text": message,
+        "timestamp": Date.now()
+    };
+    var params = {};
+    var additionalParams = {
+        headers: {},
+        queryParams: {}
+    };
+    apigClient.chatbotPost(params, body, additionalParams)
+    .then(function(result){
+        var reply = result.data.body.message;
+        addBotMsgUI(reply);
+
+    }).catch( function(err){
+        console.log(err);
+    });
+}
+
+function addUserMsgUI(reply) {
+    $("#chat-output").append(`
+    <div class='user-message'>
+    <div class='message'>
+    ${reply}
+    </div>
+    </div>
+    `);
+}
+
+function addBotMsgUI(message) {
+    $("#chat-output").append(`
+    <div class='bot-message'>
+    <div class='message'>
+    ${message}
+    </div>
+    </div>
+    `);
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $("#user-input").val("");
+}
+
+function tokenizeUrl() {
+    var vars = {};
+    var parts = window.location.href.replace(/[#?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
